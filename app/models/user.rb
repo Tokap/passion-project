704b5b133f4eb695d05b0f90e5cd0d7496c3a329
Patrick Allen <allenpatrick86@gmail.com>
2016-06-13 22:12:35 -0700

    add user migration and helper features

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,14 +6,16 @@ class User < ActiveRecord::Base
   validates :username, :email, :password_digest, { presence: true }
   validates :email, uniqueness: true
 
-  def password
-    @password ||= BCrypt::Password.new(password_digest)
-  end
+  has_secure_password
 
-  def password=(new_password)
-    @password = BCrypt::Password.create(new_password)
-    self.password_digest = @password
-  end
+  # def password
+  #   @password ||= BCrypt::Password.new(password_digest)
+  # end
+
+  # def password=(new_password)
+  #   @password = BCrypt::Password.create(new_password)
+  #   self.password_digest = @password
+  # end
 
   def self.authenticate(given_email, given_password)
     if user = self.find_by(email: given_email)
