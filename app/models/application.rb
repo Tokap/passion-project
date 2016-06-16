65f49798a491d856336ab89481b4426534132bad
Patrick Allen <email": null>
2016-06-16 16:45:06 -0700

    add days_since_application feature

--- app/models/application.rb
+++ app/models/application.rb
@@ -2,8 +2,11 @@ class Application < ActiveRecord::Base
 	belongs_to :user
   # Remember to create a migration!
 
-  def days_since_creation
-    time_passed = Date.today - date_applied
-    return time_passed
+  def days_since_application
+    days_passed = (Date.today - date_applied).to_i
+    return "New" if days_passed < 7
+    return "Hot" if days_passed >= 7 && days_passed <= 14
+    return "Frozen" if days_passed > 35
+    return "Cold" if days_passed > 14
   end
 end
